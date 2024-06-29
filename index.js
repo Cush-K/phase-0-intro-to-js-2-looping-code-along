03998cfdb8b10d7cdc0e8ba4522ee2016f468c1b
Kalvine <kkalvine4@gmail.com>
2024-06-29 23:56:54 +0300

    Adds For and While Looping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//for loop
+
+function writeCards(names, events){
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+       const greeting = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+       messages.push(greeting);
+    }
+    return messages;
+}
+console.log(writeCards([`Guadalupe`, `Ollie`, `Aki`], `surprise`));
+
+
+//while loop
+
+function countDown(){
+let count = 10;
+while (count>=0){
+    console.log(count--);
+}
+}
+console.log(10);
